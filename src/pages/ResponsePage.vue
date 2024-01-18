5583656cb0f185765b77bd5f75d1150687b91427
maxim.irmscher <maxim.irmscher@idealo.de>
2024-01-18 14:24:10 +0100

    create response done

--- src/pages/ResponsePage.vue
+++ src/pages/ResponsePage.vue
@@ -47,16 +47,14 @@ export default {
   methods: {
     handleResponseSubmitted() {
 
-      // Diese Methode wird aufgerufen, wenn eine Antwort eingereicht wurde.
-      // Hier kannst du die Antwort speichern und zur nächsten Frage wechseln.
-      // Implementiere deine Logik zur Antwortspeicherung hier.
+
 
       if (this.currentQuestionIndex < this.questions.length - 1) {
-        // Gehe zur nächsten Frage
         this.currentQuestionIndex++;
       } else {
-        // Alle Fragen wurden beantwortet
-        // Du kannst hier weitere Aktionen durchführen oder den Abschluss der Umfrage signalisieren
+        alert("Umfrage abgeschlossen Vielen Dank")
+        this.$router.push({ name: 'RespondentPage', params: {isAnonymous: this.isAnonymous} });
+
         console.log("Umfrage abgeschlossen");
       }
     },
