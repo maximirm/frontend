678eb713816301665f50a53ab954a4af5b9a2128
maxim.irmscher <maxim.irmscher@idealo.de>
2024-01-20 12:34:47 +0100

    refactor

---
+++ src/components/catalogs/UserCatalogWithExport.vue
@@ -0,0 +1,46 @@
+<template>
+  <CatalogExport
+      :exportData="mappedUserData"
+      :label="label"
+      :pdfColumnDefinition="pdfColumnDefinition">
+    <UserCatalog
+        :users="users"
+        :selectedUser="selectedUser"
+        @userSelected="selectFunction"/>
+  </CatalogExport>
+</template>
+<script>
+import CatalogExport from "@/components/catalogs/CatalogExport.vue"
+import UserCatalog from "@/components/catalogs/UserCatalog.vue"
+
+export default {
+  name: 'UserCatalogWithExport',
+  components: {CatalogExport, UserCatalog},
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    mappedUserData: {
+      type: Array,
+      required: true,
+    },
+    pdfColumnDefinition: {
+      type: Array,
+      required: true,
+    },
+    selectFunction: {
+      type: Function,
+      required: true,
+    },
+    selectedUser: {
+      type: Array,
+      required: true,
+    },
+    users: {
+      type: Array,
+      required: true,
+    }
+  }
+}
+</script>
