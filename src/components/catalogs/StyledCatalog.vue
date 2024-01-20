cfc7bf481025c74540bc641f80728505384b661a
maxim.irmscher <maxim.irmscher@idealo.de>
2024-01-20 12:37:21 +0100

    refactor

---
+++ src/components/catalogs/StyledCatalog.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="styled-catalog">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+.styled-catalog {
+  width: 500px;
+  overflow-y: auto;
+  height: 600px;
+  scrollbar-width: thin;
+  scrollbar-color: #555 #444;
+  flex-grow: 1;
+}
+
+.styled-catalog::-webkit-scrollbar {
+  width: 8px;
+}
+
+.styled-catalog::-webkit-scrollbar-thumb {
+  background-color: #555;
+}
+
+.styled-catalog::-webkit-scrollbar-thumb:hover {
+  background-color: #777;
+}
+</style>
