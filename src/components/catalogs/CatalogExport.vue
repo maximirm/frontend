bdc30d1669cae362feb372e49cbc80708479f661
maxim.irmscher <maxim.irmscher@idealo.de>
2024-01-20 12:31:39 +0100

    refactor

---
+++ src/components/catalogs/CatalogExport.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="catalog-export">
+    <slot></slot>
+    <FileExport
+        :pdfColumnDefinition="pdfColumnDefinition"
+        :fileName="label"
+        :exportData="exportData"/>
+  </div>
+</template>
+
+<script>
+import FileExport from "@/components/export/FileExport.vue";
+
+export default {
+  components: {
+    FileExport,
+  },
+  props: {
+    pdfColumnDefinition: {
+      type: Array,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    exportData: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.catalog-export {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
