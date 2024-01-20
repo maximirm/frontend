1d87dd6cd72816c79256bae03b9de04ea7f27691
maxim.irmscher <maxim.irmscher@idealo.de>
2024-01-20 12:38:42 +0100

    refactor

---
+++ src/components/catalogs/QuestionCatalog.vue
@@ -0,0 +1,37 @@
+<template>
+  <StyledCatalog>
+    <QuestionCard
+        v-for="(question, index) in questions"
+        :key="index"
+        :question="question"
+        :isSelected="selectedQuestion && selectedQuestion.id === question.id"
+        @questionSelected="selectQuestion(question)"/>
+  </StyledCatalog>
+</template>
+
+<script>
+import QuestionCard from "@/components/cards/QuestionCard.vue";
+import StyledCatalog from "@/components/catalogs/StyledCatalog.vue";
+
+export default {
+  components: {
+    StyledCatalog,
+    QuestionCard,
+  },
+  props: {
+    questions: {
+      type: Array,
+      required: true
+    },
+    selectedQuestion: {
+      type: Object,
+    },
+  },
+  methods: {
+    selectQuestion(question) {
+      this.$emit("questionSelected", question);
+    },
+  },
+};
+</script>
+
---
+++ src/components/catalogs/SurveyCatalog.vue
@@ -0,0 +1,36 @@
+<template>
+  <StyledCatalog>
+    <SurveyCard
+        v-for="(survey, index) in surveys"
+        :key="index"
+        :survey="survey"
+        :isSelected="selectedSurvey && selectedSurvey.id === survey.id"
+        @surveySelected="selectSurvey(survey)"/>
+  </StyledCatalog>
+</template>
+
+<script>
+import SurveyCard from "@/components/cards/SurveyCard.vue";
+import StyledCatalog from "@/components/catalogs/StyledCatalog.vue";
+
+export default {
+  components: {
+    StyledCatalog,
+    SurveyCard,
+  },
+  props: {
+    surveys: {
+      type: Array,
+      required: true
+    },
+    selectedSurvey: {
+      type: Object,
+    },
+  },
+  methods: {
+    selectSurvey(survey) {
+      this.$emit("surveySelected", survey);
+    },
+  },
+};
+</script>
