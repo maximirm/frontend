22f74143120d48ddae45df3043fa7952f85f6b1e
maxim.irmscher <maxim.irmscher@idealo.de>
2024-01-20 12:35:15 +0100

    refactor

---
+++ src/components/catalogs/UserCatalog.vue
@@ -0,0 +1,36 @@
+<template>
+  <StyledCatalog>
+    <UserCard
+        v-for="(user, index) in users"
+        :key="index"
+        :user="user"
+        :isSelected="selectedUser && user.id === selectedUser.id"
+        @userSelected="selectUser(user)"/>
+  </StyledCatalog>
+</template>
+
+<script>
+import UserCard from "@/components/cards/UserCard.vue";
+import StyledCatalog from "@/components/catalogs/StyledCatalog.vue";
+
+export default {
+  components: {
+    StyledCatalog,
+    UserCard,
+  },
+  props: {
+    users: {
+      type: Array,
+      required: true,
+    },
+    selectedUser: {
+      type: Object,
+    },
+  },
+  methods: {
+    selectUser(user) {
+      this.$emit("userSelected", user);
+    },
+  },
+};
+</script>
