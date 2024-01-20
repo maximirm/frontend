1ccf658441162c9b8e1e616331eb683e28bb2924
maxim.irmscher <maxim.irmscher@idealo.de>
2024-01-20 12:46:42 +0100

    refactor

---
+++ src/components/catalogs/QuestionCatalogWithExport.vue
@@ -0,0 +1,49 @@
+<template>
+  <CatalogExport
+      :exportData="mappedQuestionData"
+      :label="label"
+      :pdfColumnDefinition="pdfColumnDefinition">
+    <QuestionCatalog
+        :users="questions"
+        :selectedUser="selectedQuestion"
+        @userSelected="selectFunction"/>
+  </CatalogExport>
+</template>
+
+<script>
+import CatalogExport from "@/components/catalogs/CatalogExport.vue"
+import QuestionCatalog from "@/components/catalogs/QuestionCatalog.vue";
+
+export default {
+  components: {
+    QuestionCatalog,
+    CatalogExport,
+  },
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    mappedQuestionData: {
+      type: Array,
+      required: true,
+    },
+    pdfColumnDefinition: {
+      type: Array,
+      required: true,
+    },
+    selectFunction: {
+      type: Function,
+      required: true,
+    },
+    selectedQuestion: {
+      type: Array,
+      required: true,
+    },
+    questions: {
+      type: Array,
+      required: true,
+    }
+  }
+}
+</script>
