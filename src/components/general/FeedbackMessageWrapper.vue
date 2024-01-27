03549f0e500212a78b6f12c8cc87fa1a55670462
maxim.irmscher <maxim.irmscher@idealo.de>
2024-01-27 22:52:10 +0100

    fix styling

---
+++ src/components/general/FeedbackMessageWrapper.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="feedback-message-wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FeedbackMessageWrapper'
+};
+</script>
+
+<style scoped>
+
+.feedback-message-wrapper {
+  min-height: 30px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-co
+  ntent: center;
+}
+</style>
