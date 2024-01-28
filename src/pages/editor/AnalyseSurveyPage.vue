2435e03d275b0ac68dd3efc684eae4876406c8f0
maxim.irmscher <maxim.irmscher@idealo.de>
2024-01-28 11:10:13 +0100

    fix styling

--- src/pages/editor/AnalyseSurveyPage.vue
+++ src/pages/editor/AnalyseSurveyPage.vue
@@ -1,90 +1,70 @@
 <template>
-  <div class="analyze-survey-page">
-    <div class="list-container">
+  <BoxWrapper
+      height="800px"
+      width="500px">
+    <div class="analyse_survey-page">
+      <div class="analyse_survey-form">
+        <h2>Umfragen</h2>
+      </div>
 
       <SurveyCatalogWithExport
           :surveys="surveys"
           :selectedSurvey="selectedSurvey"
-          :selectFunction="selectSurvey"
-          class="scaled-component"/>
-      <QuestionCatalog
-          v-if="selectedSurvey && selectedSurvey.questions.length > 0"
-          :questions="questions"
-          :selectedQuestion="selectedQuestion"
-          @questionSelected="selectQuestion"
-          class="scaled-component"/>
-      <div class="fixed-panel">
-        <AnalysePanel
-            v-if="analysisComplete"
-            :barChartData="responseAnalysis"
-            :barChartLabel="'Antworten'"
-            :bar-chart-is-disabled="isFreeTextQuestion"
-            :pieChartData="respondentsAnalysis"
-            :pieChartLabel="'Befragte'"
-            :analysisIsComplete="analysisComplete"
-            class="scaled-component"/>
+          :selectFunction="selectSurvey"/>
+      <div class="button-container">
 
+        <FeedbackMessageWrapper>
+          <FeedbackMessage
+              v-if="message"
+              :messageType="'success'"
+              :message="message"/>
+        </FeedbackMessageWrapper>
+        <StyledButton
+            :label="'Umfrage löschen'"
+            :onClickMethod="deleteSelectedSurvey"
+            :isDisabled="!selectedSurvey"
+            :class="'red-btn'"/>
+        <StyledButton
+            :label="'Fragen anzeigen'"
+            :onClickMethod="showQuestions"
+            :isDisabled="!selectedSurvey || selectedSurvey.questions.length === 0"
+            :class="'green-btn'"/>
       </div>
     </div>
-    <FeedbackMessageWrapper>
-    <FeedbackMessage
-        v-if="message"
-        :messageType="'success'"
-        :message="message"/>
-    </FeedbackMessageWrapper>
-      <StyledButton
-          :label="'Umfrage löschen'"
-          :onClickMethod="deleteSelectedSurvey"
-          :isDisabled="!selectedSurvey"
-          :class="'red-btn'"/>
-      <StyledButton
-          :label="'Diagramme anzeigen'"
-          :onClickMethod="analyseQuestion"
-          :isDisabled="!selectedQuestion || selectedQuestion.responses.length === 0"
-          :class="'green-btn'"/>
-    </div>
+  </BoxWrapper>
 </template>
 
+
 <script>
-import AnalysePanel from "@/components/analysis/AnalysisPanel.vue";
 import FeedbackMessage from "@/components/generic/FeedbackMessage.vue";
 import {deleteSurvey, fetchSurveysByCreatorId} from "@/scripts/api/surveyApi";
-import {fetchAnalysedQuestion} from "@/scripts/api/analysisApi";
 import StyledButton from "@/components/buttons/StyledButton.vue";
-import SurveyCatalogWithExport from "@/components/catalogs/SurveyCatalogWithExport.vue";
-import QuestionCatalog from "@/components/catalogs/QuestionCatalog.vue";
 import FeedbackMessageWrapper from "@/components/generic/FeedbackMessageWrapper.vue";
+import SurveyCatalogWithExport from "@/components/catalogs/SurveyCatalogWithExport.vue";
+import BoxWrapper from "@/components/generic/BoxWrapper.vue";
 
 export default {
   components: {
-    FeedbackMessageWrapper,
-    QuestionCatalog,
+    BoxWrapper,
     SurveyCatalogWithExport,
+    FeedbackMessageWrapper,
     StyledButton,
     FeedbackMessage,
-    AnalysePanel,
   },
   data() {
     return {
       surveys: [],
       selectedSurvey: null,
       message: '',
-      questions: [],
-      selectedQuestion: null,
-      analysisComplete: false,
-      responseAnalysis: [],
-      respondentsAnalysis: [],
     };
-  },
-  computed: {
-    isFreeTextQuestion() {
-      return this.selectedQuestion === null || this.selectedQuestion.type === 1;
-    },
   },
   mounted() {
     this.displaySurveys();
   },
   methods: {
+    selectQuestion(question) {
+      this.selectedQuestion = question;
+    },
     async displaySurveys() {
       try {
         const creatorId = this.$store.state.userId;
@@ -99,36 +79,20 @@ export default {
             })
         )
       } catch (error) {
+        this.surveys = [];
         console.error('Fehler beim Abrufen der Umfragen:', error);
       }
     },
-    selectQuestion(question) {
-      this.responseAnalysis = [];
-      this.respondentsAnalysis = [];
-      this.analysisComplete = false;
-      this.selectedQuestion = question;
-    },
+
     selectSurvey(survey) {
-      this.analysisComplete= false;
+      this.analysisComplete = false;
       this.selectedSurvey = survey;
       this.questions = this.selectedSurvey.questions;
     },
 
-    async analyseQuestion() {
-      if (!this.selectedQuestion) {
-        return;
-      }
-      try {
-        const token = this.$store.state.userToken;
-
-        const analyzedQuestionResponse = await fetchAnalysedQuestion(token, this.selectedQuestion.id);
-
-        this.responseAnalysis = analyzedQuestionResponse.analysis_responses;
-        this.respondentsAnalysis = analyzedQuestionResponse.analysis_respondents;
-        this.analysisComplete = true;
-      } catch (error) {
-        this.analysisComplete = false;
-      }
+    showQuestions() {
+      this.$store.commit('setSelectedSurvey', this.selectedSurvey);
+      this.$router.push({name: 'AnalyseQuestionsPage'});
     },
     async deleteSelectedSurvey() {
       if (!this.selectedSurvey) return;
@@ -153,43 +117,27 @@ export default {
 
 <style scoped>
 
-.analyze-survey-page {
+.analyse_survey-page {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 80vh;
+  overflow: hidden;
 
 }
 
-
-
-
-
-.list-container {
-  display: flex;
-  gap: 10px;
-}
-
-
-.fixed-panel {
-  position: sticky;
-  top: 0;
-  right: 0;
-
-  width: 500px;
-
-  overflow-y: auto;
-  z-index: 1;
+.analyse_survey-form {
+  text-align: center;
 }
 
 .button-container {
   display: flex;
-}
-
-.scaled-component {
-  transform: scale(0.8); /* Ändere den Skalierungsfaktor nach Bedarf */
-  transform-origin: center; /* Ändere den Ursprung der Transformation nach Bedarf */
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
 }
 
 </style>
+
